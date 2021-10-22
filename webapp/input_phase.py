6f2c00c9a908571ba64322be451a5ca9d39837eb
Vasco27 <55382803+Vasco27@users.noreply.github.com>
2021-10-22 16:22:10 +0100

    Changed requirements

--- webapp/input_phase.py
+++ webapp/input_phase.py
@@ -1,10 +1,19 @@
 import streamlit as st
 
+default_tweets = """
+ts fred continues to weaken as it moves away from the cape verde islands.
+tropical storm grace forms in eastern atlantic.
+ts grace is strengthening with 50 mph winds however the system is expected to weaken in the extended forecast.
+tropical storm grace packing wind gusts of 58 mph.
+tropical storm grace will likely dissipate east of the lesser antilles.
+grace is likely to weaken to a tropical rainstorm prior to reaching the lesser antilles.
+"""
+
 def draw_sidebar():
     # Language
     lang_name_format = {'Portuguese' : 'pt', 'English' : 'en'}
     st.sidebar.header('Language')
-    lang = st.sidebar.radio('', ('Portuguese', 'English'))
+    lang = st.sidebar.radio('', ('English', 'Portuguese'))
     lang = lang_name_format[lang]
 
     # Tools
@@ -12,25 +21,25 @@ def draw_sidebar():
 
     st.sidebar.subheader('Actor entity extraction')
     ACTOR_ENTITY_EXTRACTION_TOOLS = {'spacy' : False, 'nltk' : False}
-    ACTOR_ENTITY_EXTRACTION_TOOLS['spacy']    = st.sidebar.checkbox('spaCy', '1')
-    ACTOR_ENTITY_EXTRACTION_TOOLS['nltk']     = st.sidebar.checkbox('NLTK', '2')
+    ACTOR_ENTITY_EXTRACTION_TOOLS['spacy']    = st.sidebar.checkbox('spaCy', key='1', value=True)
+    ACTOR_ENTITY_EXTRACTION_TOOLS['nltk']     = st.sidebar.checkbox('NLTK', key='2', value=False)
     # ACTOR_ENTITY_EXTRACTION_TOOLS['sparknlp'] = st.sidebar.checkbox('Spark NLP', '3')
 
     st.sidebar.subheader('Time entity extraction')
     TIME_ENTITY_EXTRACTION_TOOLS = {'py_heideltime' : False}
-    TIME_ENTITY_EXTRACTION_TOOLS['py_heideltime'] = st.sidebar.checkbox('py_heideltime', '4')
+    TIME_ENTITY_EXTRACTION_TOOLS['py_heideltime'] = st.sidebar.checkbox('py_heideltime', key='4', value=True)
 
     st.sidebar.subheader('Event entity extraction')
     EVENT_ENTITY_EXTRACTION_TOOLS = {'allennlp' : False}
-    EVENT_ENTITY_EXTRACTION_TOOLS['allennlp'] = st.sidebar.checkbox('Allen NLP', key='5')
+    EVENT_ENTITY_EXTRACTION_TOOLS['allennlp'] = st.sidebar.checkbox('Allen NLP', key='5', value=True)
 
     st.sidebar.subheader('Objectal link extraction')
     OBJECTAL_LINK_EXTRACTION_TOOLS  = {'allennlp' : False}
-    OBJECTAL_LINK_EXTRACTION_TOOLS['allennlp'] = st.sidebar.checkbox('Allen NLP', key='6')
+    OBJECTAL_LINK_EXTRACTION_TOOLS['allennlp'] = st.sidebar.checkbox('Allen NLP', key='6', value=True)
 
     st.sidebar.subheader('Semantic Role link extraction')
     SEMANTICROLE_LINK_EXTRACTION_TOOLS = {'allennlp' : False}
-    SEMANTICROLE_LINK_EXTRACTION_TOOLS['allennlp'] = st.sidebar.checkbox('Allen NLP', key='7')
+    SEMANTICROLE_LINK_EXTRACTION_TOOLS['allennlp'] = st.sidebar.checkbox('Allen NLP', key='7', value=True)
 
     return lang, {'actor_extraction_tools'              : [tool for tool in ACTOR_ENTITY_EXTRACTION_TOOLS if ACTOR_ENTITY_EXTRACTION_TOOLS[tool]],
                   'time_extraction_tools'               : [tool for tool in TIME_ENTITY_EXTRACTION_TOOLS if TIME_ENTITY_EXTRACTION_TOOLS[tool]],
@@ -77,7 +86,7 @@ def app():
     lang, tools = draw_sidebar()
 
     publication_time = str(st.date_input('Publication Date'))
-    narrative_text = st.text_area(label='Narrative Text', height=300)
+    narrative_text = st.text_area(label='Narrative Text', value=default_tweets, height=300)
 
     pressed_extract = st.button('Extract!')
 
